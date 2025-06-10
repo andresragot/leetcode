a09ea4bfe88a1be72a7803153be6bc62ef9938f4
Andrés Ragot <andresragot99@gmail.com>
2025-06-10 09:06:11 +0200

    2235. Add Two Integers

---
+++ 2235. Add Two Integers/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * LeetCode Problem: 2236. Root Equals Sum of Children
+ * Problem Link: https://leetcode.com/problems/add-two-integers/
+ * Author: Andrés Ragot (github.com/andresragot)
+ * Date: 2025-06-10
+ */
+
+class Solution 
+{
+public:
+    int sum(int num1, int num2) 
+    {
+        if (-100 > num1)
+            num1 = -100;
+        
+        if (100 < num2)
+            num2 = 100;
+
+        return num1 + num2;
+    }
+};

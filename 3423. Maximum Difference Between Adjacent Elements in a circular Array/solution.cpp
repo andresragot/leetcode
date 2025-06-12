20aa4d5d2819e0ace4a4e0f9541842255fff1b98
Andrés Ragot <andresragot99@gmail.com>
2025-06-12 14:08:28 +0200

    3423. Maximum Difference Between Adjacent Elements in a Circular Array

---
+++ 3423. Maximum Difference Between Adjacent Elements in a circular Array/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * LeetCode Problem: 3342. Maximum Difference Between Even and Odd Frequency I
+ * Problem Link: https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/
+ * Author: Andrés Ragot (github.com/andresragot)
+ * Date: 2025-06-10
+ */
+
+class Solution 
+{
+public:
+    int maxAdjacentDistance(vector<int>& nums) 
+    {
+        int max_num = INT_MIN;
+
+        for (int i = nums.size () - 1; i >= 0; --i)
+        {
+            int current = nums [i];
+            int adjacent = nums [nums.size () - 1];
+            if (i != 0)
+                adjacent = nums [i - 1];
+
+            max_num = max (abs (adjacent - current), max_num);
+        }
+        
+
+        return max_num;
+    }
+};
